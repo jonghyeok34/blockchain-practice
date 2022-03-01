a3eefaaa58a1dae2e24e5aa5ffc51fe7de6088f9
jonghyeok34 <jonghyeok34@gmail.com>
2022-03-01 18:33:07 +0900

    add nft deploy(1)

---
+++ nft-demo/README.md
@@ -0,0 +1,8 @@
+# reference
+1. open zepplin erc721
+- https://docs.openzeppelin.com/contracts/2.x/api/token/erc721
+2. nft-mix
+- https://github.com/PatrickAlphaC/nft-mix
+
+3. store data decentralized
+- ipfs
---
+++ nft-demo/scripts/deploy_and_create.py
@@ -0,0 +1,14 @@
+from scripts.helpful_scripts import get_account
+from brownie import SimpleCollectible
+
+sample_token_uri = "ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+OPENSEA_URI = "https://testnets.opensea.io/assets/{}/{}"
+
+def main():
+    account = get_account()
+    print(f"account:{account}")
+    simple_collectible = SimpleCollectible.deploy({"from": account})
+    tx = simple_collectible.createCollectible(sample_token_uri, {"from": account})
+    tx.wait(1)
+    print(f"Awsome, you can view your NFT at {OPENSEA_URI.format(simple_collectible.address, simple_collectible.tokenCounter() -1 )}")
+    print("Please wait up to 20 minutes, and hit the refresh metadata button.")
