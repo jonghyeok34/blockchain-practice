4277d8933ff243192a3a8bf3f3f9687234d46d60
jonghyeok34 <jonghyeok34@gmail.com>
2022-04-07 22:12:19 +0900

    resolve upgrade err

--- upgrades/scripts/deploy_and_upgrade.py
+++ upgrades/scripts/deploy_and_upgrade.py
@@ -12,10 +12,10 @@ from brownie import (
 def main():
     account = get_account()
     print(f"Deploying to {network.show_active()}")
-    box = Box.deploy({"from": account})
+    box = Box.deploy({"from": account}, publish_source=True)
     print(box.retrieve())
 
-    proxy_admin = ProxyAdmin.deploy({"from": account})
+    proxy_admin = ProxyAdmin.deploy({"from": account}, publish_source=True)
 
     # intializer = box.store, 1
     box_encoded_initializer_function = encode_function_data()
@@ -25,6 +25,7 @@ def main():
         proxy_admin,
         box_encoded_initializer_function,
         {"from": account, "gas_limit": 1000000},
+        publish_source=True,
     )
 
     print(f"Proxy deployed to {proxy}, you can now upgrade to v2!")
@@ -34,9 +35,14 @@ def main():
 
     # upgrade
     box_v2 = BoxV2.deploy({"from": account})
-    upgrade_transaction = upgrade(account, proxy, box_v2.address, proxy_admin_contract=proxy_admin)
+    upgrade_transaction = upgrade(
+        account,
+        proxy,
+        box_v2.address,
+        proxy_admin_contract=proxy_admin
+    )
     upgrade_transaction.wait(1)
     print("Proxy has been upgraded!")
     proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
-    proxy_box.increment({"from":account})
+    proxy_box.increment({"from": account})
     print(proxy_box.retrieve())
