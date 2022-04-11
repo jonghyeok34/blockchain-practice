d4b046ee318b23b9300024090f38097f63d6837f
jonghyeok34 <jonghyeok34@gmail.com>
2022-04-12 00:15:51 +0900

    make solidity work (compile)

--- defi-stake-yield-brownie/contracts/DappToken.sol
+++ defi-stake-yield-brownie/contracts/DappToken.sol
@@ -5,6 +5,5 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract DappToken is ERC20 {
     constructor() public ERC20("Dapp Token", "DAPP"){
         _mint(msg.sender, 1000000*10*18); 
-        
     }
 }
